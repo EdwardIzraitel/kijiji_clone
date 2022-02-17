984839a8a650daa31fe1cc6b6a5add4ffcd73641
Edward Izraitel <edward.izraital@hotmail.com>
2022-02-16 19:50:17 -0500

    ec2 upload

--- front-end/src/app/store.js
+++ front-end/src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import filterReducer  from '../features/filterSlicer'
-// import {combineReducers } from 'redusx' 
+// import {combineReducers } from 'redusx' ss
 export default configureStore({
   reducer: {
     filter: filterReducer
