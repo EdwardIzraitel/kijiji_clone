655349264c0b1d741ef7fab6cf4ab2c5a6dd2e8e
Edward Izraitel <edward.izraital@hotmail.com>
2022-02-17 08:59:18 -0500

    test ec2 instance update

--- front-end/src/app/store.js
+++ front-end/src/app/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
 import filterReducer  from '../features/filterSlicer'
-// import {combineReducers    } from 'redus  x' ss
 export default configureStore({
   reducer: {
     filter: filterReducer
