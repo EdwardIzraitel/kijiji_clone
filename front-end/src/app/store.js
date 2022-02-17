8c6ea966f16384f5dfe5f3c6ad82d9a757f1e6d0
Edward Izraitel <edward.izraital@hotmail.com>
2022-02-17 07:28:31 -0500

    github action testing

--- front-end/src/app/store.js
+++ front-end/src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import filterReducer  from '../features/filterSlicer'
-// import {combineReducers } from 'redus  x' ss
+// import {combineReducers    } from 'redus  x' ss
 export default configureStore({
   reducer: {
     filter: filterReducer
