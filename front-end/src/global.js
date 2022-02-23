cd2bfbc01c2d78e98c0790c2998510342f3d2d45
Edward Izraitel <edward.izraital@hotmail.com>
2022-02-23 07:26:58 -0500

    new link

--- front-end/src/global.js
+++ front-end/src/global.js
@@ -1,4 +1,4 @@
-const host = "http://ec2-3-99-163-219.ca-central-1.compute.amazonaws.com"
+const host = "http://ec2-3-99-187-141.ca-central-1.compute.amazonaws.com"
 // const host = "http://localhost:3000"
 export default host
 
